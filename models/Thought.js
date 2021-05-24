3a712707a7dfeb5588ee6aed760321ce8b074b3c
Tess Austin <tessaustin92@gmail.com>
2021-05-24 14:08:38 -0600

    added thought file to models folder

--- models/Thought.js
+++ models/Thought.js
@@ -1,2 +1,35 @@
 const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+// create the Thought model using the thoughtSchema
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
